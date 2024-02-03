0f36970644b97f64d93bf34039f462dca30475d6
parththakkar1818 <121672669+parththakkar1818@users.noreply.github.com>
2024-02-03 17:52:32 +0530

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow all origins for simplicity. In production, you should specify the allowed origins explicitly.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # replace with the list of allowed origins if you know them
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/sum")
+def calculate_sum(num1: int, num2: int):
+    result = num1 + num2
+    return {"result": result}
